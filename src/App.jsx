7056c150761f1b802c508b1b38d2141807ddd6c9
JenningsOmit --global <hojennings@hotmail.fr>
2025-01-08 15:44:35 +0100

    frontend ShopEase

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        {/* <Route path="/login" element={<Login/>}></Route>
+        <Route path="/product/:id" element={<ProductDetails/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const Home = () => {
+    const [ products, setProducts] = useState([]);
+
+
+    useEffect( () =>{
+        
+    })
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Home;
